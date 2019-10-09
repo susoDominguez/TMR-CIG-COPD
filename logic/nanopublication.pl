f6734e4e7e04538e6079743b6ac51c88305c3515
Dominguez Alvarez <k1214757@KCLC02S1385FVH8.local>
2019-10-09 09:43:12 +0100

    minor changes to NonDrugs

--- logic/nanopublication.pl
+++ logic/nanopublication.pl
@@ -5,26 +5,28 @@ assert_Nanopublication(Label, AgentURI, Method, NanopubURI) :-
     	% define URI for the main/head named_graph
         concat_atom([Label, '_head'], HeadID),
         rdf_global_id(data:HeadID, HeadURI),
-    	% define URI for the nanopublication
-        concat_atom([Label, 'nanopub'], NanopubID),
-        rdf_global_id(data:NanopubID, NanopubURI),
-		rdf_assert(NanopubURI, rdf:type, nanopub:'Nanopublication', HeadURI),
-    	% define URI for the assertion graph
-        concat_atom([Label, '_assertion'], AssertionID),
-        rdf_global_id(data:AssertionID, AssertionURI),
-		rdf_assert(NanopubURI, nanopub:'hasAssertion', AssertionURI, HeadURI),
+    	% define URI for the nanopublication: JESUS: now its same as head
+        % concat_atom([Label, '_nano'], NanopubID), 
+        % rdf_global_id(data:NanopubID, NanopubURI),
+		rdf_assert(HeadURI, rdf:type, nanopub:'Nanopublication', HeadURI), %before:NanopubURI
+    	% define URI for the assertion graph. JESUS, now its just the label
+        % concat_atom([Label, '_assertion'], AssertionID),
+        % rdf_global_id(data:AssertionID, AssertionURI),
+        rdf_global_id(data:Label, AssertionURI),
+		% rdf_assert(NanopubURI, nanopub:'hasAssertion', AssertionURI, HeadURI),
+        rdf_assert(HeadURI, nanopub:'hasAssertion', AssertionURI, HeadURI),
     	% define URI for the provenance graph
         concat_atom([Label, '_provenance'], ProvID),
         rdf_global_id(data:ProvID, ProvURI),
-		rdf_assert(NanopubURI, nanopub:'hasProvenance', ProvURI, HeadURI),
+		rdf_assert(HeadURI, nanopub:'hasProvenance', ProvURI, HeadURI),
     	% define URI for the publicationinfo graph
         concat_atom([Label, '_publicationinfo'], PubInfoID),
         rdf_global_id(data:PubInfoID, PubInfoURI),
-		rdf_assert(NanopubURI, rdf:type, nanopub:'Nanopublication', HeadURI),
-		rdf_assert(NanopubURI, nanopub:'hasPublicationInfo', PubInfoURI, HeadURI),
-		rdf_assert(NanopubURI, prov:'generatedAtTime', PubInfoURI, PubInfoURI), %% Function for timestamp!!!
-		rdf_assert(NanopubURI, prov:'wasAttributedTo', AgentURI, PubInfoURI),
-		rdf_assert(NanopubURI, prov:'wasGeneratedBy', Method, PubInfoURI).
+		rdf_assert(HeadURI, rdf:type, nanopub:'Nanopublication', HeadURI),
+		rdf_assert(HeadURI, nanopub:'hasPublicationInfo', PubInfoURI, HeadURI),
+		rdf_assert(HeadURI, prov:'generatedAtTime', PubInfoURI, PubInfoURI), %% Function for timestamp!!!
+		rdf_assert(HeadURI, prov:'wasAttributedTo', AgentURI, PubInfoURI),
+		rdf_assert(HeadURI, prov:'wasGeneratedBy', Method, PubInfoURI).
 												%sub:dataset_extraction
 
 
@@ -47,13 +49,13 @@ assertProvResourceUsed(NanopubURI, Resource) :-
 
 /**********************************/
 deleteNanopublication(NanopubURI) :-
-    rdf(NanopubURI, nanopub:'hasProvenance', ProvURI, HeadURI),
-    rdf(NanopubURI, nanopub:'hasAssertion', AssertionURI, HeadURI),
-	rdf(NanopubURI, nanopub:'hasPublicationInfo', PubInfoURI, HeadURI),
+    rdf(NanopubURI, nanopub:'hasProvenance', ProvURI, NanopubURI),
+    rdf(NanopubURI, nanopub:'hasAssertion', AssertionURI, NanopubURI),
+	rdf(NanopubURI, nanopub:'hasPublicationInfo', PubInfoURI, NanopubURI),
     rdf_retractall(_, _, _, PubInfoURI),
     rdf_retractall(_, _, _, ProvURI),
     rdf_retractall(_, _, _, AssertionURI),
-    rdf_retractall(NanopubURI, nanopub:'hasProvenance', ProvURI, HeadURI),
-    rdf_retractall(NanopubURI, nanopub:'hasAssertion', AssertionURI, HeadURI),
-	rdf_retractall(NanopubURI, nanopub:'hasPublicationInfo', PubInfoURI, HeadURI).
+    rdf_retractall(NanopubURI, nanopub:'hasProvenance', ProvURI, NanopubURI),
+    rdf_retractall(NanopubURI, nanopub:'hasAssertion', AssertionURI, NanopubURI),
+	rdf_retractall(NanopubURI, nanopub:'hasPublicationInfo', PubInfoURI, NanopubURI).
 
