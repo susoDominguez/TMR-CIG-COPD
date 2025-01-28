c6618d5a2acaae8222c3d065649758e58c099036
Jesus Dominguez <jesus.dominguez@kcl.ac.uk>
2025-01-28 10:06:27 +0000

    edited preTMR4i reasoner knowledge

---
+++ logic/newFiles/main.pl
@@ -0,0 +1,11 @@
+:- use_module(library(semweb/turtle)).
+:- use_module(library(semweb/rdf_http_plugin)).
+:- use_module(library(semweb/rdf_db)).
+:- use_module(library(semweb/rdfs)).
+:- use_module(library(dcg/basics)).
+
+:- include(kb_rules).
+
+:- inferInternalInteractions. 
+
+
